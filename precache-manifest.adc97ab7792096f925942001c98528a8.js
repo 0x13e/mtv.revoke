0fe5f881d26332ab8fdf7aa5d901ca440de18aaf
0x13e <0x13e@gmail.com>
2021-12-14 17:45:20 -0800

    Updates

---
+++ precache-manifest.adc97ab7792096f925942001c98528a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9a9dcc001c58d9c3d27832716e9b12",
+    "url": "/mtv.revoke/index.html"
+  },
+  {
+    "revision": "8759f2e6d09f79ae7666",
+    "url": "/mtv.revoke/static/css/2.eec3eed8.chunk.css"
+  },
+  {
+    "revision": "687180fb1d9d6c30e508",
+    "url": "/mtv.revoke/static/css/main.7a5c2649.chunk.css"
+  },
+  {
+    "revision": "8759f2e6d09f79ae7666",
+    "url": "/mtv.revoke/static/js/2.e36a4c71.chunk.js"
+  },
+  {
+    "revision": "108a60c27eadaa508472b8d4f0071d59",
+    "url": "/mtv.revoke/static/js/2.e36a4c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687180fb1d9d6c30e508",
+    "url": "/mtv.revoke/static/js/main.b283ee93.chunk.js"
+  },
+  {
+    "revision": "d202ffebcf421a76a823",
+    "url": "/mtv.revoke/static/js/runtime-main.c170d88b.js"
+  },
+  {
+    "revision": "e2b737b5c65e325cbdf832cf678d6e7e",
+    "url": "/mtv.revoke/static/media/delete.e2b737b5.png"
+  },
+  {
+    "revision": "41c86f655fc59c6e4cdae51b6b072857",
+    "url": "/mtv.revoke/static/media/erc721.41c86f65.png"
+  },
+  {
+    "revision": "f9a3852db7ae0a3e68b29bba58edfa36",
+    "url": "/mtv.revoke/static/media/mrc20.f9a3852d.png"
+  }
+]);
